517aae344c434ae84aaa1e6a275f66238c9aebd0
Hemanth B <hemanth22hemu@gmail.com>
2020-05-21 12:39:21 +0530

    introducing var data type

--- Program.cs
+++ Program.cs
@@ -5,7 +5,11 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
+            var x = 34.2;
+            var y = 10.33
+            var result = x+y;
+            Console.WriteLine(result);
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
