4bc106f7eb441d3727033bdc52398b87296c9823
Hemanth B <hemanth22hemu@gmail.com>
2020-05-24 13:17:07 +0530

    fix(program.cs): lists fixing
    ##[error]Program.cs(18,30): error CS0246: The type or namespace name 'List<>' could not be found (are you missing a using directive or an assembly reference?) [/home/runner/work/HelloWorld/HelloWorld/GradeBook.csproj]

--- Program.cs
+++ Program.cs
@@ -13,11 +13,9 @@ namespace GradeBook
             ///double[] numbers = new double[1];
             ///var numbers = new double[1];
             var numbers = new[] {12.7,10.3,6.11,4.1};
-            ///List<double> grades = new List<double>();
-            //grades.Add(12.7);
             var grades = new List<double>() {12.7,10.3,6.11,4.1};
             var result = 0.0;
-            foreach(var number in numbers)
+            foreach(var number in grades)
             {
                 result += number;
             }
