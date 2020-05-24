ff721084e59059a1145bfe08a03095f1ddeb495d
Hemanth B <hemanth22hemu@gmail.com>
2020-05-24 12:00:03 +0530

    feat(program.cs): arrays

--- Program.cs
+++ Program.cs
@@ -11,9 +11,12 @@ namespace GradeBook
             ///var result = x + y;
             ///Console.WriteLine(result);
             ///double[] numbers = new double[1];
-            var numbers = new double[1];
-            numbers[0] = 12.7;
-            Console.WriteLine($"{numbers[0]}");
+            ///var numbers = new double[1];
+            var numbers = new[] {12.7,10.3,6.11,4.1};
+            var result = numbers[0];
+            result += numbers[1];
+            result += numbers[2];
+            Console.WriteLine(result);
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
