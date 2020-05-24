e5425fecddc925cfa2e509caeb685fadad3d26d8
Hemanth B <hemanth22hemu@gmail.com>
2020-05-24 12:42:18 +0530

    fix(program.cs): declaring number
     Program.cs(18,28): error CS0230: Type and identifier are both required in a foreach statement

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace GradeBook
             var numbers = new[] {12.7,10.3,6.11,4.1};
             var result = 0.0;
             
-            foreach(number in numbers)
+            foreach(var number in numbers)
             {
                 result += number;
             }
