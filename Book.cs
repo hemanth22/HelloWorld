6456c902340c51ec51a11c1ae52403e5269d9ba1
hemanth22 <hemanth22hemu@gmail.com>
2020-06-14 22:09:34 +0530

    fix: some changes

--- Book.cs
+++ Book.cs
@@ -17,11 +17,10 @@ class Book
 
         public void ShowStatistic()
         {
-            var grades = new List<double>() {12.7,10.3,6.11,4.1};
-            grades.Add(56.1);
             var result = 0.0;
             var lowGrade = double.MinValue;
             var highGrade = double.MaxValue;
+
             foreach(var number in grades)
             {
                 lowGrade = Math.Min(number, lowGrade);
@@ -31,24 +30,12 @@ class Book
             result /= grades.Count;
 
             Console.WriteLine($"The average grade is {result}");
-            Console.WriteLine($"The average grade in 2 decimals {result:N2}");
-            Console.WriteLine($"The average grade in 3 decimals {result:N3}");
             Console.WriteLine($"The lowest grade is {lowGrade}");
             Console.WriteLine($"The highest grade is {highGrade}");
-            
-           /* if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else 
-            {
-                Console.WriteLine("Hello!");
-            }
-            */
+
         }
 
-        public List<double> grades; //granting public access
-        ///private List<double> grades; //granting public access
+        private List<double> grades;
         private string name;
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,22 +7,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            ///var x = 34.2;
-            ///var y = 10.33;
-            ///var result = x + y;
-            ///Console.WriteLine(result);
-            ///double[] numbers = new double[1];
-            ///var numbers = new double[1];
-            ///var numbers = new[] {12.7,10.3,6.11,4.1};
             var book = new Book("Bitroid's Grade Book");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
-            book.AddGrade(1.5);
+            book.AddGrade(77.5);
             book.ShowStatistic();
-            /*book.grades.Add(101); in-accessiable method
-            book.grades.Add(101); //Accessable after public method
-            */
-
         }
     }
 }
